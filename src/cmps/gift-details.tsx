a1d7fea25cd0d03706d3dfbe6fb0a71091a74a9e
Keren Siebner <kerens969@gmail.com>
2023-03-09 13:55:18 +0200

    Implemented Formik in Gift-Details component

--- src/cmps/gift-details.tsx
+++ src/cmps/gift-details.tsx
@@ -1,27 +1,72 @@
 import { ClickProps } from "../models/click-props";
 import { Store } from "../models/store";
 import { GiftCardPreview } from "./gift-card-preview";
+import {
+    Formik,
+    FormikHelpers,
+    FormikProps,
+    Form,
+    Field,
+    FieldProps,
+} from 'formik';
+
+interface MyFormValues {
+    senderFullName: string,
+    senderEmail: string,
+    personalMsg?: string,
+    recipeintFullName: string,
+    recipeintEmail: string,
+}
 
 // export function GiftDetails({selectedStore}:Store, {backToStoreList}:any){
 export function GiftDetails({ selectedStore }: Store) {
-    console.log('selectedStore', selectedStore)
+    const logoUrl = selectedStore.RegularStoreLogo
+    const initialValues: MyFormValues = { senderFullName: '', senderEmail: '', personalMsg: '', recipeintFullName: '', recipeintEmail: '' };
+
     // function onClickBack(){
     //     backToStoreList()
     // }
-    const logoUrl = selectedStore.RegularStoreLogo
-    console.log('logoUrl', logoUrl)
+
     return <div className="gift-details-container">
         <div className="select-gift-card-container">
-            <img src={logoUrl} alt="" />
+            <img className="store-logo" src={logoUrl} alt="" />
             <span>{selectedStore.StoreName} eGift Card</span>
             <GiftCardPreview store={selectedStore} />
+            <h5>{selectedStore.StoreText}</h5>
             <h4>Select gift card amount</h4>
-            {selectedStore.Products?.map((product) => (<button>
+            {selectedStore.Products?.map((product, idx) => (<button key={idx}>
                 {Math.round(product.Price)}
             </button>))}
+            {/* <a href="">Gift card details</a> */}
         </div>
         <div className="recepient-form-container">
-            <input type="text" />
+            <Formik
+                initialValues={initialValues}
+                onSubmit={(values, actions) => {
+                    console.log({ values, actions });
+                    alert(JSON.stringify(values, null, 2));
+                    actions.setSubmitting(false);
+                }}
+            >
+                <Form>
+                    <label htmlFor="senderFullName">Who is it from?</label>
+                    <span>
+                        <Field id="senderFullName" name="senderFullName" placeholder="Your Name" />
+                        <Field id="senderEmail" name="senderEmail" placeholder="Your Email" />
+                    </span>
+
+                    <label htmlFor="fullName">Presonal message</label>
+                    <Field as="textarea" id="personalMsg" name="personalMsg" placeholder="Add a personal message (optional)" />
+
+                    <label htmlFor="recipeintFullName">Who is it for?</label>
+                    <span>
+                        <Field id="recipeintFullName" name="recipeintFullName" placeholder="Their Name" />
+                        <Field id="recipeintEmail" name="recipeintEmail" placeholder="Their Email or Cell Phone" />
+                    </span>
+                    <span></span>
+                    <button type="submit" className="submit-btn">Continue</button>
+                </Form>
+            </Formik>
 
         </div>
         {/* <button onClick={onClickBack}>Back</button>  */}
